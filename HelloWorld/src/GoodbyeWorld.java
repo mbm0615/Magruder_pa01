77f9da53662d145071d3f429287eca6aa974f5d5
Matthew Magruder <mmag0615@gmail.com>
2020-01-17 22:51:35 -0500

    ah ha

--- HelloWorld/src/GoodbyeWorld.java
+++ HelloWorld/src/GoodbyeWorld.java
@@ -1,10 +1,9 @@
-package helloWorld;
 
 public class GoodbyeWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Goodbye, world!");
+System.out.println("Goodbye, World!");
 	}
 
 }
--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,10 +1,9 @@
-package helloWorld;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		System.out.println("Hello, World!");
 	}
 
 }
